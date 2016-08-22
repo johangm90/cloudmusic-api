30167b3bb8ee129003c4674c8975d5501963885f
Johan Guerreros <johangm90@gmail.com>
2016-08-22 11:18:38 -0500

    test

--- app/ApiController.php
+++ app/ApiController.php
@@ -52,7 +52,8 @@ class ApiController {
       $this->app->reroute($data['data'][0]['url']);
     }else {
       //$this->app->error(404);
-      $this->app->reroute('http://vulgry.com/rapsody/api.php?go=' . $song_id .'&quality=lMusic');
+      //$this->app->reroute('http://vulgry.com/rapsody/api.php?go=' . $song_id .'&quality=lMusic');
+      $this->app->reroute('@mp3(@song_id={$song_id},@br={$br}');
     }
   }
 
@@ -75,6 +76,33 @@ class ApiController {
     $result = $this->api->detail($song_id);
     header('content-type: application/json; charset=utf-8');
     echo $result;
+  }
+
+  function mp3() {
+    $song_id = $this->app->get('PARAMS.song_id');
+    $br = $this->app->get('PARAMS.br');
+    $result = $this->api->detail($song_id);
+    $data = json_decode($result, true);
+    switch ($br) {
+      case 96000:
+        $quality = 'l';
+        break;
+      case 160000:
+        $quality = 'm';
+        break;
+      case 320000:
+        $quality = 'h';
+        break;
+      default:
+        $quality = 'l';
+        break;
+    }
+    $sid = $data['songs'][0][$quality]['fid'];
+    $fid = $this->api->decrypt_id($sid);
+    //$url = 'http://219.138.27.38/m2.music.126.net/'.$fid.'/'.$sid.'.mp3';
+    $url = 'http://p3.music.126.net/'.$fid.'/'.$sid.'.mp3';
+    //echo $url;
+    $this->app->reroute($url);
   }
 }
 
