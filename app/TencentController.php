fe9d8e3485efeab41c1baa7e47afeb732b0086b6
Johan Guerreros <johangm90@gmail.com>
2016-11-17 14:22:11 -0500

    qq lyric

--- app/TencentController.php
+++ app/TencentController.php
@@ -48,20 +48,6 @@ class TencentController {
     }
   }
 
-  function download() {
-    $song_id = $this->app->get('PARAMS.song_id');
-    $br = $this->app->get('PARAMS.br');
-    $result = $this->api->url($song_id, $br);
-    $data = json_decode($result, true);
-    if($data['data'][0]['url']!=null){
-      echo $data['data'][0]['url'];
-    }else {
-      //$this->app->error(404);
-      $url = file_get_contents('http://vulgry.com/rapsody/api.php?download=' . $song_id .'&quality=lMusic');
-      echo $url;
-    }
-  }
-
   function detail() {
     $song_id = $this->app->get('PARAMS.song_id');
     $result = $this->api->detail($song_id);
@@ -69,31 +55,11 @@ class TencentController {
     echo $result;
   }
 
-  function mp3() {
+  function lyric() {
     $song_id = $this->app->get('PARAMS.song_id');
-    $br = $this->app->get('PARAMS.br');
-    $result = $this->api->detail($song_id);
-    $data = json_decode($result, true);
-    switch ($br) {
-      case 96000:
-        $quality = 'l';
-        break;
-      case 160000:
-        $quality = 'm';
-        break;
-      case 320000:
-        $quality = 'h';
-        break;
-      default:
-        $quality = 'l';
-        break;
-    }
-    $sid = $data['songs'][0][$quality]['fid'];
-    $fid = $this->api->decrypt_id($sid);
-    //$url = 'http://219.138.27.38/m2.music.126.net/'.$fid.'/'.$sid.'.mp3';
-    $url = 'http://p3.music.126.net/'.$fid.'/'.$sid.'.mp3';
-    //echo $url;
-    $this->app->reroute($url);
+    $result = $this->api->lyric($song_id);
+    header('content-type: application/json; charset=utf-8');
+    echo $result;
   }
 }
 
