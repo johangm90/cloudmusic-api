4a3b8e6f88e9faed751ef717237684161fbd8ba5
Johan Guerreros <johangm90@gmail.com>
2017-05-11 00:21:42 -0500

    fix get img method

--- app/ApiController.php
+++ app/ApiController.php
@@ -108,7 +108,7 @@ class ApiController {
   public function lyric() {
     $song_id = $this->app->get('PARAMS.song_id');
     $result = $this->api->lyric($song_id);
-    $lyric = $result['lrc']['lyric'];
+    $lyric = $result["lrc"]['lyric'];
     echo $lyric;
   }
 
@@ -122,7 +122,7 @@ class ApiController {
   public function img() {
     $song_id = $this->app->get('PARAMS.song_id');
     $result = $this->api->detail($song_id);
-    echo 'http://p4.music.126.net/'.$this->api->Id2Url($result['songs'][0]['al']['pic']).'/'.$result['songs'][0]['al']['pic'].'.jpg';
+    echo 'http://p4.music.126.net/'.$this->api->Id2Url($result["songs"][0]['al']['pic']).'/'.$result['songs'][0]['al']['pic'].'.jpg';
   }
 
   public function decrypt_id($id){
