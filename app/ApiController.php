aba4166491b637cf310ce817a9f27db4d266212a
Johan Guerreros <johangm90@gmail.com>
2017-05-23 22:08:07 -0500

    detail test

--- app/ApiController.php
+++ app/ApiController.php
@@ -51,7 +51,8 @@ class ApiController {
     $song_id = $this->app->get('PARAMS.song_id');
     //$br = $this->app->get('PARAMS.br');
     $br = 320;
-    $result = $this->api->url($song_id, $br);
+    //$result = $this->api->url($song_id, $br);
+    $result = $this->meting->url($song_id, $br);
     $data = json_decode($result, true);
     if($data['data'][0]['url']!=null){
       $this->app->reroute($data['data'][0]['url']);
