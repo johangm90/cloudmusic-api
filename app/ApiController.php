966e50ab929962f8b50f7b25e2a498d450fc399e
Johan Guerreros <johangm90@gmail.com>
2017-05-23 21:59:55 -0500

    detail test

--- app/ApiController.php
+++ app/ApiController.php
@@ -39,15 +39,18 @@ class ApiController {
     //mMusic = 160000
     //lMusic = 96000
     $song_id = $this->app->get('PARAMS.song_id');
-    $br = $this->app->get('PARAMS.br');
-    $result = $this->api->url($song_id, $br);
+    //$br = $this->app->get('PARAMS.br');
+    $br = 320;
+    //$result = $this->api->url($song_id, $br);
+    $result = $this->meting->url($song_id, $br);
     header('content-type: application/json; charset=utf-8');
     echo $result;
   }
 
   public function play() {
     $song_id = $this->app->get('PARAMS.song_id');
-    $br = $this->app->get('PARAMS.br');
+    //$br = $this->app->get('PARAMS.br');
+    $br = 320;
     $result = $this->api->url($song_id, $br);
     $data = json_decode($result, true);
     if($data['data'][0]['url']!=null){
@@ -82,7 +85,7 @@ class ApiController {
 
   public function detail() {
     $song_id = $this->app->get('PARAMS.song_id');
-    $result = $this->meting->url($song_id);
+    $result = $this->meting->song($song_id);
     header('content-type: application/json; charset=utf-8');
     echo $result;
   }
