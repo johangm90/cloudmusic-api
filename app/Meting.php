30fdbd056ef5dae66f247c2495b29baace4a919e
Johan Guerreros <johangm90@gmail.com>
2017-05-23 22:34:50 -0500

    detail test

--- app/Meting.php
+++ app/Meting.php
@@ -46,7 +46,7 @@ class Meting
                 $API['url']=$API['url'].'?'.http_build_query($API['body']);
             }
         }
-        $proxy = '198.47.104.134';
+        $proxy = '114.64.222.65';
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_TIMEOUT, 20);
         curl_setopt($curl, CURLOPT_ENCODING, 'gzip');
