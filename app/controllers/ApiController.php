766731a04afa21e5db66c74d7dca2153c0f83fbd
Johan Guerreros <johangm90@gmail.com>
2017-07-03 15:10:07 -0500

    rewrite api using tencent

---
+++ app/controllers/ApiController.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+* @Author: Johan Guerreros <johangm90@gmail.com>
+* @License: Copyright (c) 2016. All Rights Reserved.
+*/
+use Metowolf\Meting;
+class ApiController {
+
+  protected $api;
+  protected $app;
+
+  public function __construct() {
+    $this->api = new Meting('tencent');
+    $this->app = Base::instance();
+  }
+
+  public function json($data) {
+    header('content-type: application/json; charset=utf-8');
+    echo $data;
+  }
+
+  public function search() {
+    $keyword = $this->app->get('PARAMS.keyword');
+    $page = $this->app->get('PARAMS.page');
+    $limit = $this->app->get('PARAMS.limit');
+    $result = $this->api->format(true)->search($keyword, $page, $limit);
+    $this->json($result);
+  }
+
+  public function song() {
+    $id = $this->app->get('PARAMS.id');
+    $result = $this->api->format(true)->song($id);
+    $this->json($result);
+  }
+
+  public function album() {
+    $id = $this->app->get('PARAMS.id');
+    $result = $this->api->format(true)->album($id);
+    $this->json($result);
+  }
+
+  public function artist() {
+    $id = $this->app->get('PARAMS.id');
+    $result = $this->api->format(true)->artist($id);
+    $this->json($result);
+  }
+
+  public function playlist() {
+    $id = $this->app->get('PARAMS.id');
+    $result = $this->api->format(true)->playlist($id);
+    $this->json($result);
+  }
+
+  public function lyric() {
+    $id = $this->app->get('PARAMS.id');
+    $result = $this->api->format(true)->lyric($id);
+    $this->json($result);
+  }
+
+  public function pic() {
+    $id = $this->app->get('PARAMS.id');
+    $result = $this->api->format(true)->pic($id);
+    $this->json($result);
+  }
+
+  public function url() {
+    $id = $this->app->get('PARAMS.id');
+    $br = $this->app->get('PARAMS.br');
+    $result = $this->api->format(true)->url($id, $br);
+    $this->json($result);
+  }
+}
+
+?>
